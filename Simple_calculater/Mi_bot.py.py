6a9feeffed913c0ea7170e12b37bc219c8ba80fb
AleksandrFrl <Veselbie813@yandex.ru>
2025-08-16 03:35:49 +0300

    Update Mi_bot.py.py

--- Simple_calculater/Mi_bot.py.py
+++ Simple_calculater/Mi_bot.py.py
@@ -5,7 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hi! I crazzy?")
     
 async def main():
-    app = ApplicationBuilder().token('7814679775:AAHUfXZ-4_gDGeW2Oi7KjlV1NKX6AD-knc4').build()
+    app = ApplicationBuilder().token('').build()
     
     app.add_handler(CommandHandler("start",start))
     
